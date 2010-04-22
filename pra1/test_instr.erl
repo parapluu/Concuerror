36d0993934dc34ca65273b736a6f8f361e8ef6f5
Maria Christakis <christakismaria@gmail.com>
2010-04-22 13:08:22 +0300

    Add two tests and perform some cosmetic changes

--- pra1/test_instr.erl
+++ pra1/test_instr.erl
@@ -1,12 +1,18 @@
 -module(test_instr).
--export([test1/0, test2/0, test3/0]).
+-export([test1/0, test2/0, test3/0, test4/0, test5/0]).
 
--spec test1() -> ok.
+-spec test1() -> 'ok'.
     
 test1() ->
     Self = self(),
     %% spawn(fun() -> foo1(Self) end)
-    sched:rep_spawn(fun() -> spawn(fun() -> link(whereis(sched)), foo1(Self) end) end),
+    sched:rep_spawn(
+      fun() ->
+              spawn(fun() ->
+                            link(whereis(sched)),
+                            foo1(Self)
+                    end)
+      end),
     %% receive _Any -> ok end
     sched:rep_receive(
       fun(Aux) ->
@@ -20,30 +26,60 @@ foo1(Pid) ->
     %% Pid ! 42
     sched:rep_send(Pid, 42).
 
--spec test2() -> ok.
+-spec test2() -> 'ok'.
 
 test2() ->
     %% spawn(fun() -> foo21 end)
-    sched:rep_spawn(fun() -> spawn(fun() -> link(whereis(sched)), foo21() end) end),
+    sched:rep_spawn(
+      fun() -> 
+              spawn(fun() ->
+                            link(whereis(sched)),
+                            foo21()
+                    end)
+      end),
     %% spawn(fun() -> foo22 end)
-    sched:rep_spawn(fun() -> spawn(fun() -> link(whereis(sched)), foo22() end) end),
+    sched:rep_spawn(
+      fun() ->
+              spawn(fun() ->
+                            link(whereis(sched)),
+                            foo22()
+                    end)
+      end),
     ok.
 
 foo21() ->
     %% spawn(fun() -> foo 22 end)
-    sched:rep_spawn(fun() -> spawn(fun() -> link(whereis(sched)), foo22() end) end).
+    sched:rep_spawn(
+      fun() -> 
+              spawn(fun() ->
+                            link(whereis(sched)),
+                            foo22()
+                    end)
+      end).
 
 foo22() ->
     42.
 
--spec test3() -> ok.
+-spec test3() -> 'ok'.
 
 test3() ->
     Self = self(),
     %% spawn(fun() -> foo31 end)
-    sched:rep_spawn(fun() -> spawn(fun() -> link(whereis(sched)), foo31(Self) end) end),
+    sched:rep_spawn(
+      fun() ->
+              spawn(fun() ->
+                            link(whereis(sched)),
+                            foo31(Self)
+                    end)
+      end),
     %% spawn(fun() -> foo32 end)
-    sched:rep_spawn(fun() -> spawn(fun() -> link(whereis(sched)), foo32(Self) end) end),
+    sched:rep_spawn(
+      fun() ->
+              spawn(fun() ->
+                            link(whereis(sched)),
+                            foo32(Self)
+                    end)
+      end),
     sched:rep_receive(
       fun(Aux) ->
 	      receive
@@ -66,3 +102,54 @@ foo31(Pid) ->
 foo32(Pid) ->
     %% Pid ! msg2
     sched:rep_send(Pid, msg2).
+
+-spec test4() -> no_return().
+
+test4() ->
+    %% spawn(fun() -> foo4(Self) end)
+    sched:rep_spawn(
+      fun() ->
+              spawn(fun() ->
+                            link(whereis(sched)),
+                            foo4()
+                    end)
+      end),
+    %% receive _Any -> ok end
+    sched:rep_receive(
+      fun(Aux) ->
+	      receive
+		  _Any -> ok
+	      after 0 -> Aux()
+	      end
+      end).
+
+foo4() ->
+    %% receive _Any -> ok end
+    sched:rep_receive(
+      fun(Aux) ->
+	      receive
+		  _Any -> ok
+	      after 0 -> Aux()
+	      end
+      end).
+
+-spec test5() -> no_return().
+
+test5() ->
+    Self = self(),
+    %% spawn(fun() -> foo1(Self) end)
+    sched:rep_spawn(
+      fun() ->
+              spawn(fun() ->
+                            link(whereis(sched)),
+                            foo1(Self)
+                    end)
+      end),
+    %% receive _Any -> ok end
+    sched:rep_receive(
+      fun(Aux) ->
+	      receive
+		  43 -> ok
+	      after 0 -> Aux()
+	      end
+      end).
