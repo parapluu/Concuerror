514d65d8b65aa794fb4a50a7385dd91f1f4a5f15
Alkis Gotovos <el3ctrologos@hotmail.com>
2010-10-12 22:33:57 +0300

    Some test file changes

---
+++ resources/shootout/thread_ring.erl
@@ -0,0 +1,32 @@
+%%% The Computer Language Benchmarks Game
+%%% http://shootout.alioth.debian.org/
+%%% Contributed by Jiri Isa
+
+-module(thread_ring).
+-export([main/1, roundtrip/2, test/0]).
+
+-include("ced.hrl").
+
+-define(RING, 3).
+
+start(Token) ->
+   H = lists:foldl(
+      fun(Id, Pid) -> spawn(thread_ring, roundtrip, [Id, Pid]) end,
+      self(),
+      lists:seq(?RING, 2, -1)),
+   H ! Token,
+   roundtrip(1, H).
+
+roundtrip(Id, Pid) ->
+   receive
+      1 -> erlang:halt();
+      Token ->
+         Pid ! Token - 1,
+         roundtrip(Id, Pid)
+   end.
+
+main([Arg]) ->
+   Token = list_to_integer(Arg),
+   start(Token).
+   
+test() -> main(["3"]).
