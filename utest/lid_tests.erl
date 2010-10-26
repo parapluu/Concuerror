7842a7e8302aa3402fa0af07b4a46ab380168835
Alkis Gotovos <el3ctrologos@hotmail.com>
2010-10-27 02:17:45 +0300

    Refactored LID unit tests See #35

--- utest/lid_tests.erl
+++ utest/lid_tests.erl
@@ -14,156 +14,109 @@
 %% Spec for auto-generated test/0 function (eunit).
 -spec test() -> 'ok' | {'error', term()}.
 
-%% TODO: Should use a fixture for lid:start/stop.
 
--spec get_pid_test() -> term().
+-spec one_proc_test_() -> term().
 
-get_pid_test() ->
-    lid:start(),
-    Pid = c:pid(0, 2, 3),
-    Lid = lid:new(Pid, noparent),
-    ?assertEqual(Pid, lid:get_pid(Lid)),
-    lid:stop().
+one_proc_test_() ->
+    Setup = fun() -> lid:start(),
+ 		     Pid = c:pid(0, 2, 3),
+ 		     Lid = lid:new(Pid, noparent),
+ 		     {Pid, Lid}
+ 	    end,
+    Cleanup = fun(_Any) -> lid:stop() end,
+    Test1 = {"LID to Pid",
+	     fun({Pid, Lid}) -> ?assertEqual(Pid, lid:get_pid(Lid)) end},
+    Test2 = {"Pid to LID",
+	     fun({Pid, Lid}) -> ?assertEqual(Lid, lid:from_pid(Pid)) end},
+    Test3 = {"Parent -> Child",
+	     fun({_Pid, Lid}) ->
+		     ChildPid = c:pid(0, 2, 4),
+		     ChildLid = lid:new(ChildPid, Lid),
+		     ?assertEqual(ChildPid, lid:get_pid(ChildLid)),
+		     ?assertEqual(ChildLid, lid:from_pid(ChildPid))
+	     end},
+    Tests = [Test1, Test2, Test3],
+    Inst = fun(X) -> [{D, fun() -> T(X) end} || {D, T} <- Tests] end,
+    {foreach, local, Setup, Cleanup, [Inst]}.
 
--spec from_pid_test() -> term().
+-spec two_proc_test_() -> term().
 
-from_pid_test() ->
-    lid:start(),
-    Pid = c:pid(0, 2, 3),
-    Lid = lid:new(Pid, noparent),
-    ?assertEqual(Lid, lid:from_pid(Pid)),
-    lid:stop().
+two_proc_test_() ->
+    Setup = fun() -> lid:start(),
+ 		     Pid1 = c:pid(0, 2, 3),
+		     Pid2 = c:pid(0, 2, 4),
+ 		     Lid1 = lid:new(Pid1, noparent),
+ 		     Lid2 = lid:new(Pid2, Lid1),
+ 		     {Pid1, Pid2, Lid1, Lid2}
+ 	    end,
+    Cleanup = fun(_Any) -> lid:stop() end,
+    Test1 = {"Fold Pids",
+	     fun({Pid1, Pid2, _Lid1, _Lid2}) ->
+		     Fun = fun(P, A) -> [P|A] end,
+		     Result = lid:fold_pids(Fun, []),
+		     ?assertEqual([Pid2, Pid1], Result)
+	     end},
+    Test2 = {"Cleanup",
+	     fun({Pid1, _Pid2, Lid1, Lid2}) ->
+		     lid:link(Lid1, Lid2),
+		     lid:monitor(Lid1, Lid2, make_ref()),
+		     lid:cleanup(Lid1),
+		     ?assertEqual(not_found, lid:from_pid(Pid1)),
+		     ?assertEqual(not_found, lid:get_pid(Lid1)),
+		     ?assertEqual(0, ?SETS:size(lid:get_linked(Lid2))),
+		     ?assertEqual(0, ?SETS:size(lid:get_monitored_by(Lid2)))
+	     end},
+    Tests = [Test1, Test2],
+    Inst = fun(X) -> [{D, fun() -> T(X) end} || {D, T} <- Tests] end,
+    {foreach, local, Setup, Cleanup, [Inst]}.
 
--spec parent_child_test() -> term().
+-spec three_proc_test_() -> term().
 
-parent_child_test() ->
-    lid:start(),
-    ParentPid = c:pid(0, 2, 3),
-    ChildPid = c:pid(0, 2, 4),
-    ParentLid = lid:new(ParentPid, noparent),
-    ChildLid = lid:new(ChildPid, ParentLid),
-    ?assertEqual(ChildPid, lid:get_pid(ChildLid)),
-    lid:stop().
-
--spec link_test() -> term().
-
-link_test() ->
-    lid:start(),
-    Pid1 = c:pid(0, 2, 3),
-    Pid2 = c:pid(0, 2, 4),
-    Pid3 = c:pid(0, 2, 5),
-    Lid1 = lid:new(Pid1, noparent),
-    Lid2 = lid:new(Pid2, Lid1),
-    Lid3 = lid:new(Pid3, Lid1),
-    lid:link(Lid1, Lid2),
-    lid:link(Lid2, Lid3),
-    Set = ?SETS:from_list([Lid1, Lid3]),
-    ISection = ?SETS:subtract(Set, lid:get_linked(Lid2)),
-    ?assertEqual(0, ?SETS:size(ISection)),
-    lid:stop().
-
--spec unlink_test() -> term().
-
-unlink_test() ->
-    lid:start(),
-    Pid1 = c:pid(0, 2, 3),
-    Pid2 = c:pid(0, 2, 4),
-    Pid3 = c:pid(0, 2, 5),
-    Lid1 = lid:new(Pid1, noparent),
-    Lid2 = lid:new(Pid2, Lid1),
-    Lid3 = lid:new(Pid3, Lid1),
-    lid:link(Lid1, Lid2),
-    lid:link(Lid2, Lid3),
-    lid:unlink(Lid2, Lid1),
-    Set = ?SETS:from_list([Lid3]),
-    ISection = ?SETS:subtract(Set, lid:get_linked(Lid2)),
-    ?assertEqual(0, ?SETS:size(ISection)),
-    lid:stop().
-
--spec monitor_test() -> term().
-
-monitor_test() ->
-    lid:start(),
-    Pid1 = c:pid(0, 2, 3),
-    Pid2 = c:pid(0, 2, 4),
-    Pid3 = c:pid(0, 2, 5),
-    Lid1 = lid:new(Pid1, noparent),
-    Lid2 = lid:new(Pid2, Lid1),
-    Lid3 = lid:new(Pid3, Lid1),
-    lid:monitor(Lid1, Lid3, make_ref()),
-    lid:monitor(Lid2, Lid3, make_ref()),
-    Set = ?SETS:from_list([Lid1, Lid2]),
-    ISection = ?SETS:subtract(Set, lid:get_monitored_by(Lid3)),
-    ?assertEqual(0, ?SETS:size(ISection)),
-    lid:stop().
-
--spec demonitor_test() -> term().
-
-demonitor_test() ->
-    lid:start(),
-    Pid1 = c:pid(0, 2, 3),
-    Pid2 = c:pid(0, 2, 4),
-    Pid3 = c:pid(0, 2, 5),
-    Lid1 = lid:new(Pid1, noparent),
-    Lid2 = lid:new(Pid2, Lid1),
-    Lid3 = lid:new(Pid3, Lid1),
-    lid:monitor(Lid1, Lid3, Ref = make_ref()),
-    lid:monitor(Lid2, Lid3, make_ref()),
-    lid:demonitor(Lid1, Ref),
-    Set = ?SETS:from_list([Lid2]),
-    ISection = ?SETS:subtract(Set, lid:get_monitored_by(Lid3)),
-    ?assertEqual(0, ?SETS:size(ISection)),
-    lid:stop().
-
--spec cleanup_1_test() -> term().
-
-cleanup_1_test() ->
-    lid:start(),
-    Pid1 = c:pid(0, 2, 3),
-    Pid2 = c:pid(0, 2, 4),
-    Lid1 = lid:new(Pid1, noparent),
-    Lid2 = lid:new(Pid2, Lid1),
-    lid:link(Lid1, Lid2),
-    lid:cleanup(Lid1),
-    ?assertEqual('not_found', lid:from_pid(Pid1)),
-    ?assertEqual('not_found', lid:get_pid(Lid1)),
-    lid:stop().
-
--spec cleanup_2_test() -> term().
-
-cleanup_2_test() ->
-    lid:start(),
-    Pid1 = c:pid(0, 2, 3),
-    Pid2 = c:pid(0, 2, 4),
-    Lid1 = lid:new(Pid1, noparent),
-    Lid2 = lid:new(Pid2, Lid1),
-    lid:link(Lid1, Lid2),
-    lid:cleanup(Lid1),
-    ?assertEqual(0, ?SETS:size(lid:get_linked(Lid2))),
-    lid:stop().
-
--spec cleanup_3_test() -> term().
-
-cleanup_3_test() ->
-    lid:start(),
-    Pid1 = c:pid(0, 2, 3),
-    Pid2 = c:pid(0, 2, 4),
-    Lid1 = lid:new(Pid1, noparent),
-    Lid2 = lid:new(Pid2, Lid1),
-    lid:monitor(Lid1, Lid2, make_ref()),
-    lid:cleanup(Lid1),
-    ?assertEqual(0, ?SETS:size(lid:get_monitored_by(Lid2))),
-    lid:stop().
-
--spec fold_pids_test() -> term().
-
-fold_pids_test() ->
-    lid:start(),
-    Pid1 = c:pid(0, 2, 3),
-    Pid2 = c:pid(0, 2, 4),
-    Lid1 = lid:new(Pid1, noparent),
-    _Lid2 = lid:new(Pid2, Lid1),
-    Fun = fun(P, A) -> [P|A] end,
-    Result = lid:fold_pids(Fun, []),
-    ?assertEqual([Pid2, Pid1], Result),
-    lid:stop().
+three_proc_test_() ->
+    Setup = fun() -> lid:start(),
+ 		     Pid1 = c:pid(0, 2, 3),
+		     Pid2 = c:pid(0, 2, 4),
+		     Pid3 = c:pid(0, 2, 5),
+ 		     Lid1 = lid:new(Pid1, noparent),
+ 		     Lid2 = lid:new(Pid2, Lid1),
+		     Lid3 = lid:new(Pid3, Lid1),
+ 		     {Pid1, Pid2, Pid3, Lid1, Lid2, Lid3}
+ 	    end,
+    Cleanup = fun(_Any) -> lid:stop() end,
+    Test1 = {"Link",
+	     fun({_Pid1, _Pid2, _Pid3, Lid1, Lid2, Lid3}) ->
+		     lid:link(Lid1, Lid2),
+		     lid:link(Lid2, Lid3),
+		     Set = ?SETS:from_list([Lid1, Lid3]),
+		     ISection = ?SETS:subtract(Set, lid:get_linked(Lid2)),
+		     ?assertEqual(0, ?SETS:size(ISection))
+	     end},
+    Test2 = {"Unlink",
+	     fun({_Pid1, _Pid2, _Pid3, Lid1, Lid2, Lid3}) ->
+		     lid:link(Lid1, Lid2),
+		     lid:link(Lid2, Lid3),
+		     lid:unlink(Lid2, Lid1),
+		     Set = ?SETS:from_list([Lid3]),
+		     ISection = ?SETS:subtract(Set, lid:get_linked(Lid2)),
+		     ?assertEqual(0, ?SETS:size(ISection))
+	     end},
+    Test3 = {"Monitor",
+	     fun({_Pid1, _Pid2, _Pid3, Lid1, Lid2, Lid3}) ->
+		     lid:monitor(Lid1, Lid3, make_ref()),
+		     lid:monitor(Lid2, Lid3, make_ref()),
+		     Set = ?SETS:from_list([Lid1, Lid2]),
+		     ISection = ?SETS:subtract(Set, lid:get_monitored_by(Lid3)),
+		     ?assertEqual(0, ?SETS:size(ISection))
+	     end},
+    Test4 = {"Demonitor",
+	     fun({_Pid1, _Pid2, _Pid3, Lid1, Lid2, Lid3}) ->
+		     lid:monitor(Lid1, Lid3, Ref = make_ref()),
+		     lid:monitor(Lid2, Lid3, make_ref()),
+		     lid:demonitor(Lid1, Ref),
+		     Set = ?SETS:from_list([Lid2]),
+		     ISection = ?SETS:subtract(Set, lid:get_monitored_by(Lid3)),
+		     ?assertEqual(0, ?SETS:size(ISection))
+	     end},
+    Tests = [Test1, Test2, Test3, Test4],
+    Inst = fun(X) -> [{D, fun() -> T(X) end} || {D, T} <- Tests] end,
+    {foreach, local, Setup, Cleanup, [Inst]}.
