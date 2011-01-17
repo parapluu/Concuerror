d9e3ef8c57aaa97d6c85a0c5b3cc70d877c2064b
Alkis Gotovos <el3ctrologos@hotmail.com>
2011-01-17 19:38:03 +0200

    Updated alt_dets_bugs file

--- resources/erlang_questions/dets_bugs/alt_dets_bugs.erl
+++ resources/erlang_questions/dets_bugs/alt_dets_bugs.erl
@@ -2,22 +2,22 @@
 
 -export([bug3/0, bug5/0, bug6/0]).
 
--include("ced.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 %% should always print [{0,0}], but sometimes prints []
 bug3() ->
     dets:close(dets_table),
-    sched:rep_yield(),
+    sched:yield(),
     file:delete(dets_table),
-    sched:rep_yield(),
+    sched:yield(),
     dets:open_file(dets_table,[{type,bag}]),
-    sched:rep_yield(),
+    sched:yield(),
     spawn(fun() -> dets:open_file(dets_table,[{type,bag}]),
-                   sched:rep_yield()
+                   sched:yield()
           end),
     spawn(fun() ->
                   dets:insert(dets_table,[{0,0}]),
-                  sched:rep_yield(),
+                  sched:yield(),
                   ?assertEqual([{0,0}], get_contents(dets_table))
           end).
 
@@ -29,15 +29,15 @@ bug5() ->
                   file:delete(dets_table),
                   Parent = self(),
                   {ok, _T} = dets:open_file(dets_table,[{type,bag}]),
-                  sched:rep_yield(),
+                  sched:yield(),
                   spawn(fun() ->
                                 dets:open_file(dets_table,[{type,bag}]),
-                                sched:rep_yield(),
+                                sched:yield(),
                                 Parent ! done
                         end),
                   spawn(fun() ->
                                 dets:insert(dets_table,[{0,0}]),
-                                sched:rep_yield(),
+                                sched:yield(),
                                 ?assertEqual([{0,0}], get_contents(dets_table)),
                                 Parent ! done
                         end),
@@ -48,27 +48,27 @@ bug5() ->
 
 bug6() ->
     dets:open_file(dets_table,[{type,bag}]),
-    sched:rep_yield(),
+    sched:yield(),
     dets:close(dets_table),
-    sched:rep_yield(),
+    sched:yield(),
     dets:open_file(dets_table,[{type,bag}]),
-    sched:rep_yield(),
+    sched:yield(),
     spawn(fun() -> dets:lookup(dets_table,0),
-                   sched:rep_yield()
+                   sched:yield()
           end),
     spawn(fun() -> dets:insert(dets_table,{0,0}),
-                   sched:rep_yield()
+                   sched:yield()
           end),
     dets:insert(dets_table,{0,0}),
-    sched:rep_yield(),
+    sched:yield(),
     ?assertEqual([{0,0}], match_object(dets_table)).
 
 get_contents(Name) ->
     Ret = dets:traverse(Name, fun(X)-> {continue,X} end),
-    sched:rep_yield(),
+    sched:yield(),
     Ret.
 
 match_object(Name) ->
     Ret = dets:match_object(Name,'_'),
-    sched:rep_yield(),
+    sched:yield(),
     Ret.
