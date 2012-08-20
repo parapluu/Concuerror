dc0715f750c7e3b646493cd347710c4b4fc46ab3
Ilias Tsitsimpis <iliastsi@hotmail.com>
2012-08-20 16:39:43 +0200

    First attemp to write a new testsuite

---
+++ testsuite/runtests.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+concuerror="../concuerror"
+
+# Cleanup temp files
+find . -name '*.beam' -exec rm {} \;
+rm -rf temp
+mkdir temp
+
+# Compile scenarios.erl
+erlc scenarios.erl
+
+tests=(`ls -d suites/*/src/*`)
+
+# For every test do
+for test in "${tests[@]}"; do
+    unset files
+    unset temp
+    temp=(`echo $test | sed -e 's/suites\/\(\w\+\)\/src\/\(\w\+\)\(\.erl\)\?/\1 \2/'`)
+    suite="${temp[0]}"
+    name="${temp[1]}"
+    if [ -d $test ]; then
+        # Our test is a multi module test
+        dir=$test
+        mod="test"
+        files=(`ls $dir/*.erl`)
+    else
+        # Our test is a single module test
+        dir=${test%/*}
+        mod=$name
+        files=$test
+    fi
+    mkdir -p temp/$suite
+    # Compile it
+    erlc -W0 -o $dir $dir/$mod.erl
+    # And extract scenarios from it
+    erl -noinput -pa . -pa $dir -s scenarios extract $mod -s init stop | \
+    while read line; do
+        # Get function and preemption bound
+        unset temp
+        temp=(`echo $line | sed -e 's/{\w\+,\(\w\+\),\([0-9]\+\)}/\1 \2/'`)
+        fun="${temp[0]}"
+        preb="${temp[1]}"
+        printf "Running test %s-%s (%s, %s)..\n" $suite $name $fun $preb
+        # And run concuerror
+        $concuerror analyze --target $mod $fun --files "${files[@]}" \
+            --output temp/results.ced --preb $preb --no_progress > /dev/null
+        $concuerror show --snapshot temp/results.ced \
+            --details --all > temp/$suite/$name-$fun-$preb.txt
+        diff -uw suites/$suite/results/$name-$fun-$preb.txt \
+            temp/$suite/$name-$fun-$preb.txt
+    done
+done
+
+# Cleanup temp files
+find . -name '*.beam' -exec rm {} \;
---
+++ testsuite/scenarios.erl
@@ -0,0 +1,28 @@
+%%%----------------------------------------------------------------------
+%%% Copyright (c) 2012, Alkis Gotovos <el3ctrologos@hotmail.com>,
+%%%                     Maria Christakis <mchrista@softlab.ntua.gr>
+%%%                 and Kostis Sagonas <kostis@cs.ntua.gr>.
+%%% All rights reserved.
+%%%
+%%% This file is distributed under the Simplified BSD License.
+%%% Details can be found in the LICENSE file.
+%%%----------------------------------------------------------------------
+%%% Authors     : Tsitsimpis Ilias <iliastsi@hotmail.com>
+%%% Description : Interface to extract scenarios from our tests
+%%%----------------------------------------------------------------------
+
+-module(scenarios).
+
+-export([extract/1]).
+
+extract(Files) ->
+    S1 = lists:map(fun extractOne/1, Files),
+    S2 = lists:flatten(S1),
+    lists:foreach(fun(S) -> io:format("~p\n", [S]) end, S2).
+
+extractOne(File) ->
+    Module = list_to_atom(filename:basename(File, ".erl")),
+    %% Get the scenarios for one module
+    Scenarios = apply(Module, scenarios, []),
+    %% Put module name to it
+    lists:map(fun({Fun,Preb}) -> {Module, Fun, Preb} end, Scenarios).
