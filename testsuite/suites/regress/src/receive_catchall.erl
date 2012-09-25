6a127d2ed16163240c6254166e66df6dd6e083ed
Ilias Tsitsimpis <iliastsi@hotmail.com>
2012-09-25 22:05:35 +0300

    Add regress test case about the `catchall' bug fix

---
+++ testsuite/suites/regress/src/receive_catchall.erl
@@ -0,0 +1,40 @@
+%%%----------------------------------------------------------------------
+%%% Copyright (c) 2012, Alkis Gotovos <el3ctrologos@hotmail.com>,
+%%%                     Maria Christakis <mchrista@softlab.ntua.gr>
+%%%                 and Kostis Sagonas <kostis@cs.ntua.gr>.
+%%% All rights reserved.
+%%%
+%%% This file is distributed under the Simplified BSD License.
+%%% Details can be found in the LICENSE file.
+%%%----------------------------------------------------------------------
+%%% Authors     : Ilias Tsitsimpis <iliastsi@hotmail.com>
+%%% Description : A regress test case for the bug fix introduced
+%%%                 in commit 645ccee1a61dd1c33681544d5e02c8a4b2be0c04
+%%%----------------------------------------------------------------------
+
+-module(receive_catchall).
+-export([scenarios/0]).
+-export([test1/0, test2/0, test3/0]).
+
+scenarios() ->
+    [{test1, inf}, {test2, inf}, {test3, inf}].
+
+%% This is ok.
+test1() ->
+    self() ! hoho,
+    self() ! foo,
+    receive foo -> ok end.
+
+%% This is ok.
+test2() ->
+    Msg = foo,
+    self() ! foo,
+    self() ! hoho,
+    receive Msg -> ok end.
+
+%% This used to fail.
+test3() ->
+    Msg = foo,
+    self() ! hoho,
+    self() ! foo,
+    receive Msg -> ok end.
