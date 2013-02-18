88b51eaad52548b0b44544921030a9d5e6206dae
Ilias Tsitsimpis <i.tsitsimpis@gmail.com>
2013-02-18 11:48:52 +0200

    Add `no_tty' option to eunit tests
    This seems to solve the problem with hanging eunit
    but we have not figure out yet why the hang occurs.
    Also this options slightly decreases checked interleavings.

--- src/concuerror_rep.erl
+++ src/concuerror_rep.erl
@@ -784,5 +784,5 @@ find_pid(Other) ->
 rep_eunit(Module) ->
     ReModule = concuerror_instr:check_module_name(Module,none,0),
     rep_apply(eunit, start, []),
-    rep_apply(eunit, test, [[{module, ReModule}], [verbose]]),
+    rep_apply(eunit, test, [[{module, ReModule}], [no_tty]]),
     rep_apply(eunit, stop, []).
