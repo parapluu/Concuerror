1dc8ed776fec4827f7bd1f8bcd5d611edee2d80f
Stavros Aronis <aronisstav@gmail.com>
2013-06-17 17:36:42 +0200

    Smaller version of Manolis rush-hour, suitable for testing DPOR

--- testsuite/suites/resources/src/manolis/test.erl
+++ testsuite/suites/resources/src/manolis/test.erl
@@ -1,11 +1,15 @@
 -module(test).
 
 -export([scenarios/0]).
--export([test_2workers/0]).
+-export([test_2workers/0, test_2workers_small/0]).
 
 scenarios() ->
     [{test_2workers, 0},
-     {test_2workers, 2, dpor}].
+     {test_2workers, 2, dpor},
+     {test_2workers_small, inf, dpor}].
 
 test_2workers() ->
     rush_hour:test_2workers().
+
+test_2workers_small() ->
+    rush_hour:test_2workers_small().
