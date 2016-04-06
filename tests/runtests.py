bea500dcf2e09e0cee8dd59e2643d8d3f34e9728
Stavros Aronis <aronisstav@gmail.com>
2016-04-06 11:40:38 +0200

    Add assertion for make success in test script
    ... since we don't use the Makefile for this.

--- tests/runtests.py
+++ tests/runtests.py
@@ -143,7 +143,7 @@ concuerror = os.path.abspath(dirname + "/../concuerror")
 results = os.path.abspath(dirname + "/results")
 
 # Cleanup temp files
-os.system("make -j -C %s/.. default tests/scenarios.beam" % dirname)
+assert 0 == os.system("make -j -C %s/.. default tests/scenarios.beam" % dirname)
 os.system("rm -rf %s/*" % results)
 os.system("find . -name concuerror_report.txt -delete")
 
