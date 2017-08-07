e243b9d6d5de62b90f2a9cffb89ac510863ad040
Stavros Aronis <aronisstav@gmail.com>
2017-08-07 11:26:29 +0200

    Add a small test for message_queue_len

---
+++ tests/suites/basic_tests/src/message_queue_length.erl
@@ -0,0 +1,22 @@
+-module(message_queue_length).
+
+-export([test/0]).
+
+-export([scenarios/0]).
+
+%%------------------------------------------------------------------------------
+
+scenarios() -> [{test, inf, optimal}].
+
+%%------------------------------------------------------------------------------
+
+test() ->
+  P = self(),
+  spawn(fun() ->
+            P ! one,
+            P ! two
+        end),
+  receive
+    two ->
+      1 = erlang:process_info(self(), message_queue_len)
+  end.
