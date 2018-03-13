2b827432c92633817d2f463384317119faa9fa2c
Stavros Aronis <aronisstav@gmail.com>
2018-03-14 00:02:03 +0100

    Restore pretty printing for a crash

--- src/concuerror_dependencies.erl
+++ src/concuerror_dependencies.erl
@@ -55,7 +55,7 @@ dependent(#event{event_info = Info1, special = Special1},
           ?unique(Logger, ?lwarning, Msg, []),
           true;
         {false, _} ->
-          exit({undefined_dependency, Info1, Info2, erlang:get_stacktrace()})
+          ?crash({undefined_dependency, Info1, Info2, erlang:get_stacktrace()})
       end
   end.
 
