66d288276f3cf27fac5b4acb41dfdb772fce0a51
Stavros Aronis <aronisstav@gmail.com>
2018-04-03 14:15:16 +0200

    Allow process_info requests for system processes

--- tests/suites/basic_tests/src/inspect_system.erl
+++ tests/suites/basic_tests/src/inspect_system.erl
@@ -4,7 +4,7 @@
 -export([test/0]).
 
 scenarios() ->
-    [{test, inf, dpor, crash}].
+    [{test, inf, dpor}].
 
 test() ->
-  {group_leader, GL} = process_info(whereis(kernel_sup), group_leader).
+  {group_leader, _} = process_info(whereis(kernel_sup), group_leader).
