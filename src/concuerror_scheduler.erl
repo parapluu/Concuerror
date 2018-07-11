45ec282347a6f033f9b207c91f711be430cefe5b
Stavros Aronis <aronisstav@gmail.com>
2018-07-11 12:06:54 +0200

    Improve some debug output

--- src/concuerror_scheduler.erl
+++ src/concuerror_scheduler.erl
@@ -898,8 +898,8 @@ assign_happens_before([TraceState|Later], RevLate, RevEarly, State) ->
   %% from the step (spawns, sends and deliveries)
   FinalClockMap =
     add_new_and_messages(Special, HappenedBeforeClock, NewClockMap),
-  ?trace(_Logger, "  SC:~w~n", [StateClock]),
-  ?trace(_Logger, "  AC:~w~n", [HappenedBeforeClock]),
+  ?trace(_Logger, "       SC: ~w~n", [StateClock]),
+  ?trace(_Logger, "       AC: ~w~n", [HappenedBeforeClock]),
   NewTraceState = TraceState#trace_state{clock_map = FinalClockMap},
   assign_happens_before(Later, [NewTraceState|RevLate], RevEarly, State).
 
@@ -955,10 +955,10 @@ update_clock([TraceState|Rest], Event, Clock, State) ->
         Dependent =
           concuerror_dependencies:dependent(EarlyEvent, Event, AssumeRacing),
         ?debug(State#scheduler_state.logger,
-               "    ~s ~s~n",
+               "   ~s ~s~n",
                begin
-                 Star = fun(false) -> " ";(_) -> "*" end,
-                 [Star(Dependent), ?pretty_s(EarlyIndex,EarlyEvent)]
+                 Star = fun(false) -> "  "; (_) -> "->" end,
+                 [Star(Dependent), ?pretty_s(EarlyIndex, EarlyEvent)]
                end),
         case Dependent =:= false of
           true -> Clock;
@@ -995,9 +995,9 @@ plan_more_interleavings([TraceState|Later], RevEarly, State) ->
           ActorClock = lookup_clock(Actor, ClockMap),
           %% Otherwise we zero-down to the latest op that happened before
           LatestHBIndex = find_latest_hb_index(ActorClock, StateClock),
-          ?trace(_Logger, "   SC:~w~n", [StateClock]),
-          ?trace(_Logger, "   AC:~w~n", [ActorClock]),
-          ?debug(_Logger, "  Next @ ~w~n", [LatestHBIndex]),
+          ?trace(_Logger, "    SC: ~w~n", [StateClock]),
+          ?trace(_Logger, "    AC: ~w~n", [ActorClock]),
+          ?debug(_Logger, "    Nearest race @ ~w~n", [LatestHBIndex]),
           NewRevEarly =
             more_interleavings_for_event(
               TraceState, RevEarly, LatestHBIndex, StateClock, Later, State),
@@ -1017,12 +1017,17 @@ skip_planning(TraceState, State) ->
 more_interleavings_for_event(TraceState, RevEarly, NextIndex, Clock, Later, State) ->
   more_interleavings_for_event(TraceState, RevEarly, NextIndex, Clock, Later, State, []).
 
-more_interleavings_for_event(TraceState, [], _NextIndex, _Clock, _Later,
-                             _State, UpdEarly) ->
-  [TraceState|lists:reverse(UpdEarly)];
 more_interleavings_for_event(TraceState, RevEarly, -1, _Clock, _Later,
                              _State, UpdEarly) ->
+  ?trace(_State#scheduler_state.logger, "    Finished checking races for event~n", []),
   [TraceState|lists:reverse(UpdEarly, RevEarly)];
+more_interleavings_for_event(TraceState, [], _NextIndex, _Clock, _Later,
+                             _State, UpdEarly) ->
+  ?trace(
+     _State#scheduler_state.logger,
+     "    Finished checking races for event (NOT FAST)~n",
+     []),
+  [TraceState|lists:reverse(UpdEarly)];
 more_interleavings_for_event(TraceState, [EarlyTraceState|RevEarly], NextIndex,
                              Clock, Later, State, UpdEarly) ->
   #trace_state{
@@ -1068,7 +1073,7 @@ more_interleavings_for_event(TraceState, [EarlyTraceState|RevEarly], NextIndex,
         NC = max_cv(lookup_clock(EarlyActor, EarlyClockMap), Clock),
         ActorClock = lookup_clock(Actor, ClockMap),
         NI = find_latest_hb_index(ActorClock, NC),
-        ?debug(State#scheduler_state.logger, "  Next @ ~w~n", [NI]),
+        ?debug(State#scheduler_state.logger, "    Next nearest race @ ~w~n", [NI]),
         {NC, NI}
     end,
   {NewUpdEarly, NewRevEarly} =
@@ -1146,7 +1151,7 @@ update_trace(
                   RevV = lists:reverse(V),
                   {FixedV, ReceiveInfoDict} = fix_receive_info(RevV),
                   {FixedRest, _} = fix_receive_info(Rest, ReceiveInfoDict),
-                  show_plan(v, Logger, 0, FixedV),
+                  debug_show_sequence("v sequence", Logger, 1, FixedV),
                   case has_weak_initial_before(lists:reverse(FixedRest), FixedV, Logger) of
                     true -> {skip, false};
                     false ->
@@ -1165,7 +1170,7 @@ update_trace(
         case is_atom(Plan) of
           true -> NW;
           false ->
-            show_plan(standard, Logger, EarlyIndex, NotDep),
+            debug_show_sequence("PLAN", Logger, EarlyIndex, NotDep),
             NW
         end
     end,
@@ -1274,29 +1279,33 @@ not_obs_raw([TraceState|Rest], Later, ObserverInfo, Event, NotObs) ->
   end.
 
 has_weak_initial_before([], _, _Logger) ->
-  ?debug(_Logger, "No weak initial before~n",[]),
+  ?debug(_Logger, "    No earlier weak initials found~n",[]),
   false;
 has_weak_initial_before([TraceState|Rest], V, Logger) ->
   #trace_state{done = [EarlyEvent|Done]} = TraceState,
   case has_initial(Done, [EarlyEvent|V]) of
     true ->
-      ?debug(Logger, "Check: ~s~n",[?join([?pretty_s(0,D)||D<-Done],"~n")]),
-      show_plan(initial, Logger, 1, [EarlyEvent|V]),
+      ?debug(
+         Logger,
+         "    Has weak initial in: ~s~n",
+         [?join([?pretty_s(0, D) || D <- Done], "~n")]
+        ),
+      debug_show_sequence("if seen as", Logger, 1, [EarlyEvent|V]),
       true;
     false ->
-      ?trace(Logger, "Up~n",[]),
       has_weak_initial_before(Rest, [EarlyEvent|V], Logger)
   end.
 
-show_plan(_Type, _Logger, _Index, _NotDep) ->
+debug_show_sequence(_Type, _Logger, _Index, _NotDep) ->
   ?debug(
-     _Logger, "     PLAN (Type: ~p)~n~s",
+     _Logger, "     ~s:~n~s",
      begin
        Indices = lists:seq(_Index, _Index + length(_NotDep) - 1),
        IndexedNotDep = lists:zip(Indices, _NotDep),
+       Format = "                                       ~s~n",
        [_Type] ++
          [lists:append(
-            [io_lib:format("        ~s~n", [?pretty_s(I,S)])
+            [io_lib:format(Format, [?pretty_s(I,S)])
              || {I,S} <- IndexedNotDep])]
      end).
 
